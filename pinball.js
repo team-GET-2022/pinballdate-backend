de805192a0ad17caaa5bc51bad2abdb4f44ad8bf
Rey Mercado <reymercado.usa@gmail.com>
2022-03-08 14:10:15 -0800

    multiple endpoints to pinball api now working

--- pinball.js
+++ pinball.js
@@ -6,10 +6,13 @@ const axios = require('axios');
 async function getPinball(request, response, next) {
   try {
     let searchQuery = request.query.searchQuery;
-    let url = `https://pinballmap.com/api/v1/locations/closest_by_address.json?address=${searchQuery}`;
-    let results = await axios.get(url);
-    console.log('results ', results.data);
-    response.send(results.data.location.name);
+    // let url = `https://pinballmap.com/api/v1/locations/closest_by_address.json?address=${searchQuery}`; //works
+    let url2 = `https://pinballmap.com/api/v1/locations/closest_by_address.json?send_all_within_distance=true;no_details=1;by_machine_id=2771;address=${searchQuery};max_distance=50`; //works
+    let results = await axios.get(url2);
+    console.log('results ', results.data); //works
+    response.send(results.data.locations); //works
+    // response.send(results.data.location.name); //works
+    // response.send(results.data.location); //works
   } catch (error) {
     Promise.resolve().then(() => {
       throw new Error(error.message);
